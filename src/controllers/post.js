8687c0a827ac080d1f82364cad38ba0a8e0d3d1d
Rafael Audax <rafael@grupoaudax.com.br>
2019-07-01 21:05:15 +0000

    Create route update post by slug.

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -12,6 +12,10 @@ router.get('/', async (req, res) => {
 router.get('/:slug', async (req, res) => {
   const { slug } = req.params
 
+  if (!slug) {
+    res.status(410).json({ message: 'Slug required.' })
+  }
+
   try {
     const post = await Post.findOne({ slug })
 
@@ -39,7 +43,27 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.put('/:slug', async (req, res) => { })
+router.put('/:slug', async (req, res) => {
+  const { slug } = req.params
+  const { title, description, body } = req.body
+
+  if (!slug) {
+    res.status(410).json({ message: 'Slug required.' })
+  }
+
+  if (!title || !description || !body) {
+    res.status(410).json({ message: 'Title, Description and Body required.' })
+  }
+
+  try {
+    const post = await Post.findOneAndUpdate({ slug }, { title, description, body })
+
+    return res.json({ post })
+  } catch (error) {
+    res.status(500).json({ message: 'Server error. Try later.' })
+    console.log(error)
+  }
+})
 
 router.delete('/:slug', async (req, res) => { })
 
